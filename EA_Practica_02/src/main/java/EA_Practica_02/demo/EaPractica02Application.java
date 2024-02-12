1211236184c45e8615b8be533d6c80af7b3d6ee9
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-02-11 23:59:20 -0600

    ea-pratica-02-semana5

---
+++ EA_Practica_02/src/main/java/EA_Practica_02/demo/EaPractica02Application.java
@@ -0,0 +1,13 @@
+package EA_Practica_02.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaPractica02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaPractica02Application.class, args);
+	}
+
+}
---
+++ EA_Practica_02/src/test/java/EA_Practica_02/demo/EaPractica02ApplicationTests.java
@@ -0,0 +1,13 @@
+package EA_Practica_02.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaPractica02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
